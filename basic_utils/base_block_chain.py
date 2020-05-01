88674f3dd6f80abf86cbe3a546631ca973cf4945
Sathvic <sathvic.p@gmail.com>
2020-05-02 01:53:03 +0530

    Add methods to get last block, push to chain and basic add block to base class

--- basic_utils/base_block_chain.py
+++ basic_utils/base_block_chain.py
@@ -8,6 +8,8 @@ class BaseBlockChain():
     Handles the basic functionality of creating
     and handling addition of blocks to the chain.
     """
+    block_chain = []
+    string_encoding = 'utf-8'
 
     def __init__(self, genesis_hash=None, genesis_string=None, str_length=9):
         """
@@ -25,6 +27,7 @@ class BaseBlockChain():
         genesis_hash = genesis_hash or sha512(
             bytes(genesis_string, 'utf-8')
         ).hexdigest()
+        self.block_chain.append(genesis_hash)
 
     def get_chain(self):
         """
@@ -33,9 +36,28 @@ class BaseBlockChain():
         """
         return tuple(self.block_chain[1:])
 
-    def get_genesis_hash(self):
-        """Get the genesis block"""
+    def get_genesis_block(self):
+        """Get the genesis block signature"""
         return self.block_chain[0]
+
+    def get_last_block(self):
+        """Get the last block of the chain"""
+        return self.block_chain[-1]
+
+    def add_block(self, transaction):
+        """
+        Generate and add block where each block contains 
+        one transaction of single string type
+        """
+        prev_block = self.get_last_block()
+        new_block = sha512(bytes(prev_block, self.string_encoding))
+        self.push_block_to_chain(new_block.hexdigest())
+
+    def push_block_to_chain(self, block):
+        """
+        Push the generated block to the blockchain
+        """
+        self.block_chain.append(block)
 
     def __str__(self):
         """String representation of the block chain"""
